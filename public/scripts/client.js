c5e764cafb9902257f15767038ef469f37c1ebcf
‘Gonzo <gonzonieto@gmail.com>
2022-04-04 14:20:13 +0000

    completed all core requirements and half of stretch; handing in

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,20 +1,17 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(() => {
   // Makes a GET request to /tweets/ and returns the array of tweets
-  const loadTweets = () => {
+  const loadTweets = ((last = false) => {
     $.ajax('/tweets/', { method: 'GET' })
       .then((tweets) => {
+        if (last) {
+          renderTweets([tweets[tweets.length - 1]]);
+          return;
+        }
         // Sort so the newest tweet is displayed first
         tweets.sort((a,b) => b.created_at - a.created_at);
         renderTweets(tweets);
       });
-  };
-
+  });
   // Render existing tweets as soon as possible in the page load sequence.
   loadTweets();
   
@@ -51,32 +48,55 @@ $(document).ready(() => {
   const renderTweets = (tweets) => {
     tweets
       .map(tweetData => createTweetElement(tweetData))
-      .forEach(tweetElement => $('#tweets-container').append(tweetElement));
+      .forEach(tweetElement => $('#tweets-container').prepend(tweetElement));
   };
 
   const newTweetBox = $('.new-tweet');
   const tweetForm = $('.new-tweet > form');
-
-  // textarea of new tweet form
+  // textarea of form for new tweets
   const tweet = $("#tweet-text");
 
-  tweet.focus((e) => {
+
+  $('nav div').click(e => {
+    $('.new-tweet').slideToggle('slow', () => {
+      $('nav i').removeClass('bounce');
+      if ($('.new-tweet').is(':visible')) {
+        $('nav div span').html('<strong>Read</strong> some tweets.');
+        $("html, body").animate({ scrollTop: $(".new-tweet").offset().top - $('nav').height() - 15 }, 950);
+      } else {
+        $('nav div span').html('<strong>Write</strong> a new tweet.');
+      }
+    });
+  });
+  // $("html, body").animate({ scrollTop: $("#tweets-container").offset().top - $('nav')[0].scrollHeight }, 1500);
+
+  // Bounce the icon in the navbar on hover
+  $('nav div').hover(e => {
+    $('nav i').toggleClass('bounce');
+  });
+
+  // If user focuses on the textarea, then if there are any elements of class 'error', hide and remove them
+  tweet.focus(e => {
     if ($('.error').length) {
       $('.error').slideToggle({
         duration: 'slow',
-        start: () => {
-          $('#tweet-text').css({ 'box-shadow': '0 0 0 0', 'transition': '0.6s' });
-        },
+        start: () => $('#tweet-text').css({ 'box-shadow': '0 0 0 0', 'transition': '0.6s', 'margin': '0px 0px' }),
         done: () => {
+          $('.new-tweet h2').slideToggle('slow');
           $('.error').remove();
         }
       });
     }
   });
 
-  // $('#tweet-text').css({ 'box-shadow': '0 0 0 0', 'transition': '0.3s' });
+  // Toggle clicked state for icons in #tweets-container
+  // NOT SURE WHY THIS ISN'T WORKING, SINCE IT WORKS AS INTENDED WHEN PASTED INTO CHROME DEV TOOLS
+  // $('#tweets-container i').click(e => {
+  //   console.log(e.target);
+  //   $(e.target).toggleClass('clicked');
+  // });
 
-  tweetForm.submit((e) => {
+  tweetForm.submit(e => {
     const tc = $('#tweets-container')[0];
     e.preventDefault();
     
@@ -95,8 +115,9 @@ $(document).ready(() => {
         $('.error').slideToggle({
           duration: 'slow',
           start: () => {
-            $('#tweet-text').css({ 'box-shadow': 'rgb(85, 91, 255) 0px 0px 0px 3px, rgb(31, 193, 27) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px', 'transition': '0.4s' });
+            $('#tweet-text').css({ 'box-shadow': 'rgb(85, 91, 255) 0px 0px 0px 3px, rgb(31, 193, 27) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px', 'transition': '0.4s', 'margin': '15px 0px' });
             $('.error').css('display', 'flex');
+            $('.new-tweet h2').slideToggle('slow');
           }
         });
       }
@@ -110,107 +131,21 @@ $(document).ready(() => {
         $('.error').slideToggle({
           duration: 'slow',
           start: () => {
-            $('#tweet-text').css({ 'box-shadow': 'rgb(85, 91, 255) 0px 0px 0px 3px, rgb(31, 193, 27) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px', 'transition': '0.4s' });
+            $('#tweet-text').css({ 'box-shadow': 'rgb(85, 91, 255) 0px 0px 0px 3px, rgb(31, 193, 27) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px', 'transition': '0.4s', 'margin': '15px 0px' });
             $('.error').css({ 'display': 'flex' });
+            $('.new-tweet h2').slideToggle('slow');
           }
         });
       }
       return;
     }
-
-
-    
-    $.post("/tweets", tweetForm.serialize());
+    // $.post(url, data, successFunc)
+    $.post("/tweets", tweetForm.serialize(), () => loadTweets(true));
     
     // Clear text field and set counter back to 140
     $('#tweet-text').val('');
     $('.counter').val(140);
 
-    // Remove all child nodes from #tweets-container
-    while (tc.childElementCount) {
-      tc.removeChild(tc.firstElementChild);
-    }
-    // Reload tweets from database
-    loadTweets();
-
-    $("html, body").animate({ scrollTop: $("#tweets-container").offset().top - $('nav')[0].scrollHeight - 10 }, 1500);
+    $("html, body").animate({ scrollTop: $("#tweets-container").offset().top - $('nav')[0].scrollHeight }, 1500);
   });
 });
-
-
-
-
-
-//
-// /*
-//  * Client-side JS logic goes here
-//  * jQuery is already loaded
-//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-//  */
-
-// $(document).ready(() => {
-//   // Makes a GET request to /tweets/ and returns the array of tweets
-//   const loadTweets = (options = { last: false }) => {
-//     $.ajax('/tweets/', { method: 'GET' })
-//       .then((tweets) => {
-//         options.last
-//           // Render only the last tweet to be added to the database
-//           ? renderTweets(tweets[tweets.length - 1])
-//           // Sort from newest to oldest and render all tweets in the database
-//           : renderTweets(tweets.sort((a, b) => b.created_at - a.created_at));
-//       });
-//   };
-
-//   // Render existing tweets as soon as possible in the page load sequence.
-//   loadTweets();
-
-//   const createTweetElement = (td) => {
-//     const $tweet = $(`<article class="tweet">
-//     <header>
-//       <img src="${td.user.avatars}"></img>
-//       <span>${td.user.name}</span>
-//       <span id="handle">${td.user.handle}</span>
-//     </header>
-//     <p>
-//       ${td.content.text}
-//     </p>
-//     <footer>
-//       <span id="date">${timeago.format(td.created_at)}</span>
-//       <i class="fa-solid fa-thumbs-up"></i>
-//       <i class="fa-solid fa-retweet"></i>
-//       <i class="fa-solid fa-heart"></i>
-//     </footer>
-//     </article>`);
-//     return $tweet;
-//   };
-
-//   const renderTweets = (tweets) => {
-//     tweets
-//       .map(tweetData => createTweetElement(tweetData))
-//       .forEach(tweetElement => $('#tweets-container').append(tweetElement));
-//   };
-
-//   const tweetForm = $('.new-tweet > form');
-
-//   tweetForm.submit((e) => {
-//     const tl = $("#tweet-text").val().trim().length;
-//     const tc = $('#tweets-container')[0];
-//     e.preventDefault();
-    
-//     // Validation -- Reject empty input and input over 140 characters
-//     if (tl > 140) return alert('Tweets must contain 140 characters or less! Anything longer is technically considered a "squawk" and is not permitted by our Terms of Service.');
-//     if (!tl)      return alert('You must have something to say before you can say something.');
-    
-//     $.post("/tweets", tweetForm.serialize());
-//     $('#tweet-text').val('');
-
-//     // Remove all child nodes from #tweets-container
-//     // while (tc.childElementCount) {
-//     //   tc.removeChild(tc.firstElementChild);
-//     // }
-//     // Reload tweets from database
-//     loadTweets({ last: true });
-
-//     $("html, body").animate({ scrollTop: $("#tweets-container").offset().top - $('nav')[0].scrollHeight - 10 }, 1500);
-//   });
-// });
